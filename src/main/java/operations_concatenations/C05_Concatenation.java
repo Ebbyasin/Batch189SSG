3d2b6d7ba45cfb5c09e5c41b62b34388a956858d
yasin mesekoparan <y.mesekoparan1510@gmail.com>
2023-08-18 11:47:06 +0300

    eb

---
+++ src/main/java/operations_concatenations/C05_Concatenation.java
@@ -0,0 +1,13 @@
+package operations_concatenations;
+
+public class C05_Concatenation {
+
+    public static void main(String[] args) {
+
+        String str="Java bugun biraz kolaydi";
+        System.out.println(str+'!');
+        System.out.println(""+'J'+'a'+'v'+'a');
+        //System.out.println((String) 15+(String)26);
+
+    }
+}
--- src/main/java/scannerCarsamba/C07_scanner.java
+++ src/main/java/scannerCarsamba/C07_scanner.java
@@ -2,8 +2,7 @@ package scannerCarsamba;
 
 import java.util.Scanner;
 
-public class C06_scanner {
-
+public class C07_scanner {
     public static void main(String[] args) {
         //Soru 5) Kullanicidan ismini ve soyismini isteyip asagidaki sekilde yazdirin
         //Isminiz: Ali
@@ -14,15 +13,8 @@ public class C06_scanner {
         String isim= scan.nextLine();
         System.out.println("lutfen soyIsminizi giriniz");
         String soyIsim= scan.nextLine();
-
-
-        /*
+        int boslukIndex=isim.indexOf(" ");
         System.out.println("Isminiz: "+isim.toUpperCase().charAt(0)+isim.substring(1,boslukIndex+1).toLowerCase()+isim.split(" ")[1].toUpperCase().charAt(0)+isim.substring(boslukIndex+2).toLowerCase());
         System.out.println("SoyIsminiz: "+soyIsim.toUpperCase().charAt(0)+soyIsim.substring(1).toLowerCase());
-
-
-         */
-        System.out.println("Isminiz : " +isim.toUpperCase().charAt(0) + isim.toLowerCase().substring(1));
-        System.out.println("Soyisminiz : " +soyIsim.toUpperCase().charAt(0) + soyIsim.toLowerCase().substring(1));
     }
 }
