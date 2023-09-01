3791ac9bbb033339ba5ebb6311695f9b3b123731
yasin mesekoparan <y.mesekoparan1510@gmail.com>
2023-09-01 17:35:33 +0300

    01.09

--- src/main/java/whileLoop01/C05_WhileLoop.java
+++ src/main/java/whileLoop01/C05_WhileLoop.java
@@ -1,18 +1,19 @@
-package doWhileLoop;
+package whileLoop01;
 
 import java.util.Random;
 import java.util.Scanner;
 
-public class C05_doWhileLoop {
-
-
+public class C05_WhileLoop {
     public static void main(String[] args) {
         Random rnd=new Random();
         int sayi=rnd.nextInt(100);
         Scanner scan=new Scanner(System.in);
-        int tahminSayisi=0;
-        int kullaniciSayi;
-        do {
+        int tahminSayisi=5;
+        int kullaniciSayi=-1; //dongunun icine girebilmesi icin bir deger verilmek zorunda,o degerde random
+                              //classinin secebilecegi sayilar arasinda olmamasi lazim,o yüzden -1 sectik
+                              // do while kullandiginda sadece variable olusturmamiz yeterli,cunkü dongu her
+                              // türlü baslayacak nasilsa.
+        while (kullaniciSayi!=sayi){
             System.out.println("lutfen tahminde bulunmak icin 1-100 arasinda bir sayi giriniz");
             kullaniciSayi= scan.nextInt();
             if (kullaniciSayi>=1&&kullaniciSayi<=100){
@@ -23,7 +24,7 @@ public class C05_doWhileLoop {
                 }
             }else System.out.println("ben sana 1-100 arasinda bir sayi gir dedim!!!");
             tahminSayisi++;
-        }while (kullaniciSayi!=sayi);
+        }
         System.out.println("bilgisayarin tuttugu sayiyi : "+tahminSayisi+". tahminde buldunuz sayi : "+sayi);
     }
 }
